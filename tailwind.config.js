df9417bbe2097c6a488c00446782dafa36cddb57
MdKhairulHassan <darknessmoon76@gmail.com>
2024-12-30 00:09:08 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,10 +23,10 @@ export default {
       },
       maxWidth: {
         'headerContainer': '1200px',
-        'laptopContainer': '900px',
-        'tapletContainer': '700px',
-        'androidContainer': '500px',
-        'mobileContainer': '290px',
+        'laptopContainer': '978px',
+        'tapletContainer': '748px',
+        'androidContainer': '556px',
+        'mobileContainer': '300px',
       },
       width: {
         'LogoW': '30%',
@@ -36,21 +36,21 @@ export default {
         'UserW': '20%',
       },
       screens: {
-      'laptop': {'min': '912px', 'max': '1218px',},
-      // => @media (min-width: 1280px) { ... }
+      'laptop': {'min': '992px', 'max': '1199.98px',},
+      // => @media (min-width: 992px) { ... }
 
-      'taplet': {'min': '720px', 'max': '912px',},
-      // => @media (min-width: 720px) { ... }
+      'taplet': {'min': '768px', 'max': '991.98px',},
+      // => @media (min-width: 768px) { ... }
 
-      'android': {'min': '514px', 'max': '720px',},
-      // => @media (min-width: 514px) { ... }
+      'android': {'min': '576px', 'max': '767.98px',},
+      // => @media (min-width: 576px) { ... }
 
-      'mobile': {'min': '300px', 'max': '514px',},
-      // => @media (min-width: 300px) { ... }
+      'mobile': {'min': '320px', 'max': '575.98px',},
+      // => @media (min-width: 320px) { ... }
 
-      // 'sm': '100px',
+      // 'sm': {'min': '100px', 'max': '319.98px',},
       // // => @media (min-width: 100px) { ... }
-    },
+     },
     },
   },
   plugins: [],
